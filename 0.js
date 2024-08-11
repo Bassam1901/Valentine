a7c925cb7bbd8cea8affc8471d3677371ed88af1
Bassam Eldawy <99674889+Bassam1901@users.noreply.github.com>
2024-08-11 04:55:03 +0300

    Add files via upload

---
+++ 0.js
@@ -0,0 +1,46 @@
+const phrases = [
+    "NO",
+    "Ya 7aywana ?",
+    "Hnhzr ?",
+    "?????",
+    "Wallahi ?",
+    "Ha2ol l omk .. ",
+  ];
+
+
+
+
+
+const yesBtn = document.getElementById("yes");
+
+const noBtn = document.getElementById("no");
+
+const yayText = document.getElementById("yay");
+
+const yayText1 = document.getElementById("yay1");
+
+const can1 = document.getElementById("can1");
+
+const can = document.getElementById("can");
+
+noBtn.addEventListener("click", () => {
+yesBtn.style.width = `${yesBtn.offsetWidth + 75}px`;
+
+yesBtn.style.height = `${yesBtn.offsetHeight + 75}px`;
+X=Math.floor(Math.random()*phrases.length);
+console.log(X);
+noBtn.textContent=phrases[X];
+
+
+});
+
+yesBtn.addEventListener("click", () => {
+  yayText.style.display = "block";
+
+  yayText1.style.display = "block";
+
+  can.style.display = "none";
+
+  can1.style.display = "none";
+});
+
